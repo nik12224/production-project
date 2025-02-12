1ea8e4ec707fb359e50476424288177baa096740
Zaur <0lezgin5@gmail.com>
2025-02-12 16:29:18 +0400

    добавил тестирование jest и покрыл тестом компонент

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
 	return [
 		cls,
 		...additional.filter(Boolean),
-		Object.entries(mods)
+		...Object.entries(mods)
 			.filter(([className, value]) => Boolean(value))
 			.map(([className, value]) => className),
 	].join(' ')
