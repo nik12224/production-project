1a4e742ae193fae6724a59b1f0e2c99bfd45b8a3
Zaur <0lezgin5@gmail.com>
2025-02-15 21:58:25 +0400

    обновил sidebar, добавил для button темы

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,31 +2,38 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
-export enum ThemeButton {
-    CLEAR = 'clear',
-    OUTLINE = 'outline',
+export enum ButtonTheme {
+	CLEAR = 'clear',
+	OUTLINE = 'outline',
+	BACKGROUND = 'background',
+	BACKGROUND_INVERTED = 'backgroundInverted',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-    className?: string;
-    theme?: ThemeButton;
+export enum ButtonSize {
+	M = 'size-m',
+	L = 'size-l',
+	XL = 'size-xl',
 }
 
-export const Button: FC<ButtonProps> = (props) => {
-    const {
-        className,
-        children,
-        theme,
-        ...otherProps
-    } = props;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+	theme?: ButtonTheme;
+	square?: boolean;
+	size?: ButtonSize;
+}
 
-    return (
-        <button
-            type="button"
-            className={classNames(cls.Button, { [cls[theme]]: true }, [className])}
-            {...otherProps}
-        >
-            {children}
-        </button>
-    );
+export const Button: FC<ButtonProps> = props => {
+	const { className, children, theme, square, size = ButtonSize.M, ...otherProps } = props;
+
+	const mods: Record<string, boolean> = {
+		[cls[theme]]: true,
+		[cls.square]: square,
+		[cls[size]]: true,
+	};
+
+	return (
+		<button type="button" className={classNames(cls.Button, mods, [className])} {...otherProps}>
+			{children}
+		</button>
+	);
 };
