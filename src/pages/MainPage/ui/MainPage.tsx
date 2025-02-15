bf08c42d04bdf25e8534fd055d6a5d41cec5e811
Zaur <0lezgin5@gmail.com>
2025-02-15 17:58:31 +0400

    initial files

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,14 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const MainPage: FC = () => {
-	const { t } = useTranslation();
+const MainPage = () => {
+    const { t } = useTranslation();
 
-	return <div>{t('Главная страница')}</div>;
+    return (
+        <div>
+            {t('Главная страница')}
+        </div>
+    );
 };
 
 export default MainPage;
