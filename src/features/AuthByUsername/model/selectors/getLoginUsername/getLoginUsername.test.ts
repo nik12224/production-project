b72038d69ccc2d55eea7216d553a59f851ee11d8
Zaur <0lezgin5@gmail.com>
2025-02-21 11:08:34 +0400

    добавил тесты для модуля авторизации

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from './getLoginUsername';
+
+describe('getLoginPUsername.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'Admin',
+            },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('Admin');
+    });
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
